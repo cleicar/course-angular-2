853d981ba21e4c6a548f26bab6e8651874cd84f4
Cleiviane Costa <costa.cleiviane@gmail.com>
2017-06-20 17:31:52 -0300

    Introdução à diretivas e templates.

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'; //prepara a aplicação para ser executada por um browser.
+import { NgModule } from '@angular/core'; // decorations comuns do Angular
+import { FormsModule } from '@angular/forms'; // permite fazer data-bindins com a diretiva ng-model
+import { HttpModule } from '@angular/http'; // permite fazer requisições ajax
 
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';
 import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
-  declarations: [
+  declarations: [ // Todos componentes, diretivas e pipes do projeto.
     AppComponent,
     MeuPrimeiroComponent,
     MeuSegundoComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ // Outros modulos que serão usados neste modulo ou nos componentes usados pelo modulo.
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    CursosModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // Todos os serviços que ficarão disponíveis nos componentes do projeto.
+  bootstrap: [AppComponent] // Qual componente será instanciado quando a aplicação executar. 
+                            // Ele serve como container, é o view-port do projeto. Esta apenas no modulo raiz.
 })
+
 export class AppModule { }
